1d81a01d365bc71330ef2210d75a1a669650312f
Florian Labarre <contact@florianlabarre.fr>
2024-08-19 12:01:36 +0200

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
-import vueHtmlToPaper from './vue-html-to-paper';
 import './style.css'
 import App from './App.vue'
 
 createApp(App)
-    // .use(vueHtmlToPaper)
     .mount('#app');
